873f52a1eedd4a3085c73718305529b2261ebe2f
mazenQBS <m.fukaha@qbs.jo>
2022-07-16 16:47:18 +0300

    cleanCode

--- QBS-training/ChefFile/ArabicChef.cs
+++ QBS-training/ChefFile/ArabicChef.cs
@@ -7,7 +7,8 @@ namespace QBS_training.ChefFile
         public class ArabicChef : Chef
         {
             public override StringBuilder ToStringChefResponsible()
-            { return Help.DoneMessageFormat(new StringBuilder("The order is prepared by an expert chef in the Arabic dish")); }
+            { return Help.DoneMessageFormat(new StringBuilder("The order is" +
+                                                              " prepared by an expert chef in the Arabic dish")); }
 
         }
 
